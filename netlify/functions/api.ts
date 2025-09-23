9f35372d33fbcf18ac3dc3e5d960c778eff471f6
Dondada <andresalvator6@gmail.com>
2025-09-24 01:48:15 +0200

    feat(netlify): add serverless wrapper for express API

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,3 +1,11 @@
+import serverless from 'serverless-http';
+import { createServer } from '../../../server/index';
+
+// Create the express app once per function cold start
+const app = createServer();
+
+// Export the Netlify-compatible handler
+export const handler = serverless(app as any);
 import serverless from "serverless-http";
 
 import { createServer } from "../../server";
